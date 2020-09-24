1ea2d93d7cb8cb86c69b886789bcc0adf1e6674d
Ankita Mani <ankita.mani@decimal.co.in>
2020-09-24 20:19:29 +0530

    j

--- lib/main.dart
+++ lib/main.dart
@@ -15,47 +15,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Login Screen"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            color: Colors.black,
-            width: 350,
-            height: 350,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-
-                    width: 100,
-                    height: 100,
-                    padding: EdgeInsets.all(8),
-                    color: Colors.red,
-                  ),
-                  Container(
-
-                    width: 100,
-                    height: 100,
-                    padding: EdgeInsets.all(8),
-                    color: Colors.yellow,
-                  ),
-                  Container(
-
-                    width: 100,
-                    height: 100,
-                    padding: EdgeInsets.all(8),
-                    color: Colors.green,
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      )
+      body:Container()
     );
   }
 }
