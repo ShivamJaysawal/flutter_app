fa1671a4471ec8f5e0b9023da0abf3c04b43e100
Shivam Jaysawal <shivam.jaysawal@decimal.co.in>
2020-10-18 18:20:14 +0530

    streamTut

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/pages/home_page.dart';
@@ -7,26 +10,82 @@ import 'package:path/path.dart';
 import 'package:flutter_app/utils/Constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  Contants.prefs=await SharedPreferences.getInstance();
+ main() {
 
 
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Shivam App",
-    home:Contants.prefs.getBool("loggedIn")==true
-        ?HomePageFB()
-        :LoginPage(),
-    theme: ThemeData(
-      primarySwatch: Colors.purple
-    ),
-    routes: {
-      "/login":(BuildContext)=>LoginPage(),
-      "/home":(BuildContext)=>HomePage(),
-    },
-  ));
+  runApp(MyApp());
 
 }
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+   StreamController<double> controller=StreamController();
+   StreamSubscription streamSubscription;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Shivam App",
+        home:Scaffold(
+          body: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                MaterialButton(
+                  child: Text("Subscribe"),
+                  color: Colors.yellow,
+                  onPressed: (){
+                    Stream stream=controller.stream;
+                    streamSubscription =  stream.listen((value){
+                      print('Value from controller: $value');
+                    });
+//-----get manual stream example---------------------------------------------
+//on this way ,stream is by default broadcast, means multiple listener allowed
+                 /*  getDelayedRandomValues().listen((value){
+                      print('Value from controller: $value');
+                    });*/
+
+                  },
+
+                ),
+                MaterialButton(
+                  child: Text("Emit(Add) value"),
+                  color: Colors.blue,
+                  onPressed: (){
+                  controller.add(12);
+
+                  },
+
+                ),
+                MaterialButton(
+                  child: Text("UnSubscribe"),
+                  color: Colors.red,
+                  onPressed: (){
+                    streamSubscription?.cancel();
+                  },
+
+                )
+              ],
+            ),
+          ),
+        )
+
+
+    );
+  }
+  Stream<double> getDelayedRandomValues() async*{
+    var random=Random();
+    while(true){
+     await Future.delayed(Duration(seconds: 1));
+     yield random.nextDouble();
+    }
+
+  }
+}
+
 
 
